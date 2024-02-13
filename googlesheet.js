fd54809ac1060e222b938533e21bc7814784f87c
Ivy Fang <nibvay06@gmail.com>
2024-02-13 14:23:11 +0800

    fix: hide google sheet related

--- googlesheet.js
+++ googlesheet.js
@@ -1,28 +1,28 @@
 require("dotenv").config();
 
-const { GoogleSpreadsheet } = require("google-spreadsheet");
-const { JWT } = require("google-auth-library");
+// const { GoogleSpreadsheet } = require("google-spreadsheet");
+// const { JWT } = require("google-auth-library");
 
-const jwt = new JWT({
-  email: process.env.CLIENT_EMAIL,
-  key: process.env.PRIVATE_KEY,
-  scopes: ["https://www.googleapis.com/auth/spreadsheets"],
-});
+// const jwt = new JWT({
+//   email: process.env.CLIENT_EMAIL,
+//   key: process.env.PRIVATE_KEY,
+//   scopes: ["https://www.googleapis.com/auth/spreadsheets"],
+// });
 
 async function getSheetData() {
-  const doc = new GoogleSpreadsheet(process.env.SHEET_ID, jwt);
-  await doc.loadInfo();
+  // const doc = new GoogleSpreadsheet(process.env.SHEET_ID, jwt);
+  // await doc.loadInfo();
 
-  const sheet = doc.sheetsByIndex[0];
-  const rows = await sheet.getRows();
+  // const sheet = doc.sheetsByIndex[0];
+  // const rows = await sheet.getRows();
 
-  console.log("item: ", rows[0].get("item"), " value:", rows[0].get("value")); // date
-  console.log("item: ", rows[1].get("item"), " value:", rows[1].get("value")); // leave
-  console.log("item: ", rows[2].get("item"), " value:", rows[2].get("value")); // alternate
-  return {
-    leave: rows[1].get("value"),
-    alternate: rows[2].get("value"),
-  };
+  // console.log("item: ", rows[0].get("item"), " value:", rows[0].get("value")); // date
+  // console.log("item: ", rows[1].get("item"), " value:", rows[1].get("value")); // leave
+  // console.log("item: ", rows[2].get("item"), " value:", rows[2].get("value")); // alternate
+  // return {
+  //   leave: rows[1].get("value"),
+  //   alternate: rows[2].get("value"),
+  // };
 }
 
 function updateSheet() {
